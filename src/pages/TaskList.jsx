42cda350c4e170d3fbed8abdefc97c4954548800
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-06-13 11:04:34 +0200

    feat: enhance TaskList with table layout and add TaskRow component for task display

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,13 +1,27 @@
 import { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext.jsx";
+import TaskRow from "../components/TaskRow.jsx";
 
 export default function TaskList() {
   const { tasks } = useContext(GlobalContext);
-  console.log(tasks); // Stampa le task per verificare il corretto recupero
 
   return (
     <div>
       <h1>Pagina delle task</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Status</th>
+            <th>Data di creazione</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <TaskRow key={task.id} task={task} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
